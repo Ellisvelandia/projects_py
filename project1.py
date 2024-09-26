199239932b9aef536ef7f43e107ceb54751627d3
Ellis velandia <eyis619@gmail.com>
2024-09-25 21:45:12 -0500

    adventure

--- project1.py
+++ project1.py
@@ -7,17 +7,18 @@ should_we_play = input('Do you want to play? ').lower()
 
 if should_we_play == 'yes' or should_we_play == 'y':
     print('ok lets play')
+    weapon = input('which weapon do you want to use? (sword/knife)').lower()
     direction = input('left or right? ').lower()
     if direction == 'left':
         print('you went left and fell of a cliff, ghame over')
     elif direction == 'right':
         print('you went right , right path')
         choice = input('Okay,  you now see a bridge , do you want to swim under it or cross it? (swim/cross)').lower()
-        if choice == 'swim':
-            print('you got eaten by an alligator, game over')
+        if choice == 'swim' and weapon == 'sword':
+            print('you beaten a alligator and made it to the other side')
         else:
             print('you cross the bridge and make it to the other side')     
     else:
-        print('sorry not a valid direction')
+        print('sorry, game over')
 else:
     print('ok, see you next time')
